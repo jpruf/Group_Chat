054bdee0674884743079aee1809a2b62b286bdd8
Jennifer Prufer <jprufer@gmail.com>
2016-06-15 13:03:53 -0700

    finished files

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-// require express and path
 var express = require("express");
 var path = require("path");
 var app = express();
-// static content 
-app.use(express.static(path.join(__dirname + "./static")));
 
-// setting up ejs and our views folder
+app.use(express.static(path.join(__dirname + "./static")));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
-// root route to render the index.ejs view
 app.get('/', function(req, res) {
  res.render("index");
 })
-// tell the express app to listen on port 8000
 var server = app.listen(8000, function() {
  console.log("group chat on port 8000");
 })
@@ -29,29 +24,19 @@ io.sockets.on("connection", function(socket) {
     users[socket.id] = data.user; 
     console.log(data.user);
 
-  io.emit("new_member", {name: data.user});
-  socket.emit("all_members", {all_users: users})
-})
+    io.emit("new_member_alert", {name: data.user});
+    io.emit("all_members", {all_users: users})
+  })
 
   socket.on("disconnect", function(data){
+    console.log(users[socket.id]);
+    io.emit("disconnect_alert", {name: users[socket.id]});
     delete users[socket.id];
     socket.broadcast.emit("all_members", {all_users: users})
   })
 
-socket.on("new_message", function(data){
-
-  io.emit("")
-})
-
-  // socket.on("new_user", function(data){
-  // 	io.emit("alert_peeps", {name: username});
-  // })
-  // socket.on("new_message", function(message){
-  // 	io.emit("display_message", message);
-  // })
-  // console.log(socket.user);
-  //all the socket code goes in here!
-  // socket.broadcast.emit("new_user", function(data){
-  // 	console.log("new user: " + data.reason);
-  // })
+  socket.on("new_message", function(data){
+    console.log(data.message);
+    io.emit("display_message", {message: data.message, user: users[socket.id]});
+  })
 })
