e9d932eb8c927208fd3ce6a3854f6e3c77103116
Jennifer Prufer <jprufer@gmail.com>
2016-06-14 23:41:55 -0700

    updated member logic

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // require express and path
 var express = require("express");
 var path = require("path");
-// create the express app
 var app = express();
 // static content 
 app.use(express.static(path.join(__dirname + "./static")));
+
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
@@ -16,19 +16,39 @@ app.get('/', function(req, res) {
 var server = app.listen(8000, function() {
  console.log("group chat on port 8000");
 })
+
+var messages = [];
+var users = {};
+
 var io = require("socket.io").listen(server);
 //all code for serverside to be inside the callback passed to io.sockets.o
 io.sockets.on("connection", function(socket) {
   console.log(socket.id);
 
-  socket.on("new_user", function(username){
-  	console.log(username.user.selector);
-  	io.emit("alert_peeps", username.user.selector);
+  socket.on("new_user", function(data){
+    users[socket.id] = data.user; 
+    console.log(data.user);
+
+  io.emit("new_member", {name: data.user});
+  socket.emit("all_members", {all_users: users})
+})
+
+  socket.on("disconnect", function(data){
+    delete users[socket.id];
+    socket.broadcast.emit("all_members", {all_users: users})
   })
-  socket.on("new_message", function(message){
-  	console.log(message);
-  	io.emit("display_message", message);
-  })
+
+socket.on("new_message", function(data){
+
+  io.emit("")
+})
+
+  // socket.on("new_user", function(data){
+  // 	io.emit("alert_peeps", {name: username});
+  // })
+  // socket.on("new_message", function(message){
+  // 	io.emit("display_message", message);
+  // })
   // console.log(socket.user);
   //all the socket code goes in here!
   // socket.broadcast.emit("new_user", function(data){
